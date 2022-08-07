84bd82ff853be5225e59fe957214a3c0999e6cdc
bala3404 <40672209+bala3404@users.noreply.github.com>
2022-08-07 20:29:21 +0100

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,7 @@
 def call(Map stageParams) {
     
     sh "echo hello world"
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+  
+    
+
 }
